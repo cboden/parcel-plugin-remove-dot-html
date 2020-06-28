ed95ffa2cf78d73c8a51afc214eaa9e6d0facb74
Chris Boden <cboden@gmail.com>
2020-06-28 17:27:46 -0400

    Skip if development, added index.html replacement before other .html

--- HTMLPackager.js
+++ HTMLPackager.js
@@ -1,14 +1,11 @@
-// Package modules.
 const debug = require('debug')('parcel:remove-dot-html');
 const posthtml = require('posthtml');
 
-// Constants.
 const MATCHER_NODES = [
   {tag: 'a', attrs: {href: true}},
   {tag: 'link', attrs: {href: true}}
 ];
 
-// Exports.
 module.exports = (SuperHTMLPackager) => {
   return class HTMLPackager extends SuperHTMLPackager {
     constructor(...args) {
@@ -25,7 +22,24 @@ module.exports = (SuperHTMLPackager) => {
     }
 
     removeDotHTML(tree) {
+      if ('development' == process.env.NODE_ENV) {
+        debug('$NODE_ENV is "development", skipping %s', this.bundle.getHashedBundleName());
+
+        return tree;
+      }
+
       let count = 0;
+
+      tree.match(MATCHER_NODES, (node) => {
+        const { href } = node.attrs;
+        if (href.indexOf(this.publicURL) === 0 && href.slice(-11) === '/index.html') {
+          count += 1;
+          node.attrs.href = href.slice(0, -10); // Preserve trailing slash.
+        }
+
+        return node;
+      });
+
       tree.match(MATCHER_NODES, (node) => {
         const {href} = node.attrs;
         if (href.indexOf(this.publicURL) === 0 && href.slice(-5) === '.html') {
